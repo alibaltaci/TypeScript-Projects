b0299c6785c70864c48195ac3001a2c9646be4e2
ali baltaci <alibaltaci13@gmail.com>
2024-03-29 00:09:38 +0300

    Store items

--- advanced-shopping-cart/src/pages/Store/Store.tsx
+++ advanced-shopping-cart/src/pages/Store/Store.tsx
@@ -1,11 +1,23 @@
+import { Col, Row } from "react-bootstrap"
 import { storePageData } from "../../data/moduleData"
+import StoreItem from "../../components/StoreItem/StoreItem"
 
 const Store = () => {
 
   console.log(storePageData)
+  const {pageTitle, items} = storePageData
   return (
     <>
-      <h1>{storePageData.pageTitle}</h1>
+      <h1>{pageTitle}</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {
+          items.map( item => (
+            <Col key={item.id}>
+              <StoreItem {...item} />  
+            </Col>
+          ))
+        }
+      </Row>
     </>
   )
 }
